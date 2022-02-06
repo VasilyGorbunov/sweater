9c2e51da8247911ce7034dc8a8b31aca7c0e5810
VasilyGorbunov <vasily.gorbunov@gmail.com>
2022-02-06 23:02:34 +0300

    video 11 - 3:00

--- src/main/java/com/example/sweater/controllers/RegistrationController.java
+++ src/main/java/com/example/sweater/controllers/RegistrationController.java
@@ -1,24 +1,18 @@
 package com.example.sweater.controllers;
 
-import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
-import com.example.sweater.repos.UserRepo;
+import com.example.sweater.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
-    private final UserRepo userRepo;
-
     @Autowired
-    public RegistrationController(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -27,17 +21,11 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
 
-        if(userFromDb != null) {
+        if(!userService.addUser(user)) {
             model.put("message", "User already exists!");
             return "registration";
         }
-
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
-
         return "redirect:/login";
     }
 
--- src/main/java/com/example/sweater/services/UserService.java
+++ src/main/java/com/example/sweater/services/UserService.java
@@ -1,10 +1,14 @@
 package com.example.sweater.services;
 
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,5 +21,16 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+        if(userFromDb != null) {
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+        return true;
     }
 }
