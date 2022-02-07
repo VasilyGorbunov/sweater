c8929a036cead810cf58cb1246c108c865182ea2
VasilyGorbunov <vasily.gorbunov@gmail.com>
2022-02-07 14:07:44 +0300

    video 11 - done

--- src/main/java/com/example/sweater/services/UserService.java
+++ src/main/java/com/example/sweater/services/UserService.java
@@ -7,15 +7,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
-    private final UserRepo userRepo;
+    private UserRepo userRepo;
+    private MailSender mailSender;
 
-    public UserService(UserRepo userRepo) {
+    public UserService(UserRepo userRepo, MailSender mailSender) {
         this.userRepo = userRepo;
+        this.mailSender = mailSender;
     }
 
     @Override
@@ -30,7 +34,33 @@ public class UserService implements UserDetailsService {
         }
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
+
+        if(!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format(
+                "Hello, %s \n" +
+                "Welcome to Sweater! Please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+
+        if(user == null) {
+            return false;
+        }
+
+        user.setActivationCode(null);
+        userRepo.save(user);
+
         return true;
     }
 }
